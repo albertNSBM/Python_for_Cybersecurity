3df30926ec4406d66d072b740d0fd0cc2be0e053
albertNSBM <albertnsaba2018@gmail.com>
2024-07-21 19:15:37 +0200

    Password_generator_tool

--- Passwor_Generator.py
+++ Passwor_Generator.py
@@ -3,15 +3,16 @@
 import random
 import string 
 
+# Generating  a string where the automated pass will be generated 
 characters= string.ascii_letters+string.digits+string.punctuation
 length=13
 
-random_characters=[]
+random_characters=[]   # list that wil help us to store generated password 
 
-for i in range(length):
+for i in range(length):           # loop for generating password
 	random_character=random.choice(characters)
 	random_characters.append(random_character)
-Password= ''.join(random_characters)
+Password= ''.join(random_characters)            # using join() to join the character in the list
 
 print(Password)
 
